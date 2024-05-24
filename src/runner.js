d93cd0d4617540c1d75838d4f93052d2af1c3705
Shriram Thirukkumaran <shriram.thirukkumaran@gmail.com>
2024-05-24 14:11:47 +0530

    Refactor runner.js to handle API key configuration

--- src/runner.js
+++ src/runner.js
@@ -1,18 +1,17 @@
 const apiURL = require('./utils/api.js');
 const readUserConfig = require('./utils/config.js');
-const config = readUserConfig();
 
 const runner = function (code, options) {
+  let config = {};
+  if(!options && !options?.apiKey) {
+     config = readUserConfig();
+  } else {
+    config = options;
+  }
   if (!config.apiKey || !config.apiKey.length) {
     throw new Error(
       'API Key is required to run wax-dev. Please reach out to technology@wallyax.com to get your API Key.'
     );
-  }
-  let configRules = [];
-  if (config.rules) {
-    configRules = config.rules;
-  } else if (options) {
-    configRules = options.rules;
   }
   return new Promise((resolve, reject) => {
     fetch(apiURL + '?apiKey=' + config.apiKey, {
@@ -20,7 +19,7 @@ const runner = function (code, options) {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ element: code, rules: configRules }),
+      body: JSON.stringify({ element: code, rules: config.rules }),
     })
       .then((response) => response.json())
       .then((data) => {
