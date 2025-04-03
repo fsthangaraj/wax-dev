7669ed4822ab64afacee58b3b077e5794030a9d6
fsthangaraj <tmanthiriyappan@fleetstudio.com>
2025-04-03 21:33:01 +0530

    fix:passed only specific details

--- src/runnerHtml.js
+++ src/runnerHtml.js
@@ -22,10 +22,51 @@ const runner = function (code, options) {
       },
       body: JSON.stringify({ element: code, rules: config.rules, isLinter:"false" }),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        resolve(data);
-      })
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanedData = data.map((item) => {
+        const {
+          element,
+          message,
+          description,
+          severity,
+          code,
+          groupData = {}
+        } = item;
+    
+        const cleanedItem = {
+          element,
+          message,
+          severity,
+          grouping: groupData.grouping,
+          subgroup: groupData.subgroup,
+          why_issue: groupData.why_issue,
+          what_is_missing: groupData.what_is_missing,
+          how_to_solve: groupData.how_to_solve
+        };
+    
+        if (description) {
+          cleanedItem.description = description;
+        }
+    
+        if (groupData.example_before) {
+          cleanedItem.example_before = groupData.example_before;
+        }
+    
+        if (groupData.example_after) {
+          cleanedItem.example_after = groupData.example_after;
+        }
+    
+        if (code) {
+          cleanedItem.code = code.split('_')[0].split(',')[0];
+        }
+    
+        return cleanedItem;
+      });
+    
+      resolve(cleanedData);
+    })
+    
       .catch((error) => reject(error));
   });
 };
--- src/runnerUrl.js
+++ src/runnerUrl.js
@@ -24,7 +24,52 @@ const runner = function (code, options) {
     })
       .then((response) => response.json())
       .then((data) => {
-        resolve(data);
+        const cleanedIssues = data.issues.map((item) => {
+          const {
+            code,
+            type,
+            message,
+            context,
+            selector,
+            component,
+            issue_category_name,
+            groupData = {}
+          } = item;
+
+          const cleanedItem = {
+            type,
+            message,
+            context,
+            selector,
+            component,
+            issue_category_name,
+            grouping: groupData?.grouping,
+            subgroup: groupData?.subgroup,
+            why_issue: groupData?.why_issue,
+            what_is_missing: groupData?.what_is_missing,
+            how_to_solve: groupData?.how_to_solve
+          };
+
+          if (code) {
+            cleanedItem.code = code?.split('_')[0].split(',')[0];
+          }
+
+          if (groupData?.example_before) {
+            cleanedItem.example_before = groupData?.example_before;
+          }
+
+          if (groupData?.example_after) {
+            cleanedItem.example_after = groupData?.example_after;
+          }
+
+          return cleanedItem;
+        });
+
+        resolve({
+          documentTitle: data.documentTitle,
+          pageUrl: data.pageUrl,
+          issues: cleanedIssues
+        });
       })
       .catch((error) => reject(error));
   });
